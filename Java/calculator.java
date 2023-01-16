fd9b196d5ff4cc7a7e05ae11c653a8628c8e45e6
eakman45 <105893039+eakman45@users.noreply.github.com>
2023-01-16 19:40:38 +0300

    Add files via upload

--- Java/calculator.java
+++ Java/calculator.java
@@ -4,15 +4,12 @@ public class calculator {
     public static void main(String[] args) {
 
         int para;
-        float kdvkucukbin,kdvbuyukbin,kdvsizfiyat;
+        float kdvkucukbin,kdvbuyukbin;
         Scanner inp = new Scanner(System.in);
         System.out.println("Lütfen ödediğiniz miktarı giriniz :");
         para = inp.nextInt();
-
         kdvkucukbin = ((para * 18)/100F);
         kdvbuyukbin = ((para * 8)/100F);
-
-        System.out.println(para < 1000? "KDVsiz fiyat = "+(para-kdvkucukbin):"KDVsiz fiyat ="+(para-kdvbuyukbin));
 
         System.out.println("Ödediğiniz miktara göre sizden alınan KDV :");
         System.out.println(para < 1000?"Alınan KDV %18 : "+kdvkucukbin : "Alınan KDV %8 : "+kdvbuyukbin);
